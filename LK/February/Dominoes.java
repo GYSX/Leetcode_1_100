fd265eeb488252c39017549f769a762747f515cd
GYSXY <925124701@qq.com>
2022-02-22 11:21:33 +0800

    算法练习

--- LK/February/Dominoes.java
+++ LK/February/Dominoes.java
@@ -3,7 +3,7 @@ public class Dominoes {
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
         //String Do = cin.next();
-        String Do =".R...L.R..R.R..";
+        String Do =".RR..";
        // System.out.print("0123456789\n");
         System.out.print(Do+"\n");
         String rDo=pushDominoes(Do);
@@ -11,45 +11,46 @@ public class Dominoes {
     }
     static String pushDominoes(String dominoes) {
         char A='L', B;
-        int a = 0,b=1,c=0;
-        char[] rdominoes=new char[dominoes.length()+1];
+        int a = 0, b =0,c=1;
+        char[] rdominoes=new char[dominoes.length()];
         for(int i = 0; i < dominoes.length() ; i++){
             rdominoes[i]='.';
         }
         for(int i = 0; i < dominoes.length() ; i++){
             B = dominoes.charAt(i);
             if (B !='.') {
-                if (b == 0) {
-                    A = B;
-                    c=i;
-                    b = 1;
-                } else{
 
                     if (A=='R'&& B =='L') {
                         a = a / 2;
                         for (int j = 0; j <= a; j++) {
-                            rdominoes[c  + j] = A;
+                            rdominoes[b + j] = A;
                             rdominoes[i  - j] = B;
                         }
 
                     }else if (A== B){
+                        if (c==1)
+                            b=-1;
                             for (int j = 0; j <= a; j++) {
-                                rdominoes[c + j] = A;
+                                rdominoes[b +1+ j] = A;
                             }
 
-                    }else { }
-                    c=i;
+                    }else { rdominoes[i] = B; }
+                    b =i;
                     a=0;
                     A= B;
-                }
+                    c=2;
 
             }else
                 a++;
+            if (a == dominoes.length())return dominoes;
         }
-        if (A=='R')
+        if (A=='R') {
             for (int j = 0; j <= a; j++) {
-                rdominoes[c + j] = A;
+                rdominoes[b + j] = A;
             }
+        }
+        else
+            rdominoes[b] = A;
     return String.valueOf(rdominoes);
     }
 }
