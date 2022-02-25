ad741dde3fd3b280bec96b9c485cacb6605919c2
GYSXY <925124701@qq.com>
2022-02-25 08:28:28 +0800

    2.25-8.28

--- LK/February/回文数.java
+++ LK/February/回文数.java
@@ -3,17 +3,17 @@ public class 回文数 {
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
         int x = 121;
-        bool rx=isPalindrome(x);
+        boolean rx=isPalindrome(x);
         System.out.print(rx);
     }
-    public static bool isPalindrome(int x) {
+    public static boolean isPalindrome(int x) {
         int n=0,i=0;
         if (x<0)
         return false;
         else{
             for(;Math.pow(10,n)<=x;n++){}
         for (;n==i;){
-            if(x/Math.pow(10,n)==(x/Math.pow(10.i+1))/Math.pow(10,i)){
+            if(x/Math.pow(10,n)==(x/Math.pow(10,i+1))/Math.pow(10,i)){
                 n--;i++;
             }
             else
