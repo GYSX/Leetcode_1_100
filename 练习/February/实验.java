19f28fdbd9613d8bb8071fc1aa78c0d50e354ebf
GYSXY <925124701@qq.com>
2022-03-12 18:33:22 +0800

    2022.3.12

--- 练习/February/实验.java
+++ 练习/February/实验.java
@@ -3,11 +3,25 @@ import java.util.List;
 import java.util.Stack;
 
 public class 实验 {
-    public static void main(String[] args){
-        Stack<Integer> stack=new Stack<Integer>();
-        stack.push(0);
-        stack.push(0);
-        System.out.println(""+stack.size());
 
+    public static class ab{ }
+    public static class a extends ab{
+        int value=1;
+    }
+    public static class b extends ab{
+        int value=1;
+    }
+    public static void main(String[] args){
+        ab a=new a();
+        a A=new a();
+        ab b=new b();
+        b B=new b();
+
+        a c=new a();
+        b C=new b();
+        System.out.println(a==A); //false
+        System.out.println(a==b); //false
+        System.out.println(A==B); //false
+//        System.out.println(c==C); //编译错误
     }
 }
